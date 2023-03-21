4d956102eb6c4f72733b25658cd95722a666431f
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-03-21 15:44:29 -0700

    Updated data and sql scripts for summary

--- demo/src/main/java/com/example/demo/Repository/SegmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Segment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SegmentRepository extends CrudRepository<Segment, Integer> {
-  Segment findByRecordId(Integer recordId);
-}
--- demo/src/main/java/com/example/demo/Repository/SummaryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Summary;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SummaryRepository extends CrudRepository<Summary, Integer> {
-Summary findByRecordId(Integer recordId);
-}
