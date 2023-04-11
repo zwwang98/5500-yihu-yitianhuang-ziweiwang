014e7e9b2930b28ca9b514e7e79ecaf3c0ec16a2
7sy3 <89568830+7sy3@users.noreply.github.com>
2023-04-11 16:59:25 -0700

    frontend updated

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import './App.css';
+import Suggestion from './Suggestion';
+import Recommendation from './Recommendation';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Home = () => <h1>Hello!</h1>;
+const Ranking = () => <h1>Ranking</h1>;
+// const Suggestion = () => <h1>Suggestion</h1>;
+// const Recommendation = () => <h1>Recommendation</h1>;
+
+function App() {
+  return (
+    <Router>
+      <div className="App container">
+        <nav>
+          <Link to="/home">Home</Link>
+          <Link to="/ranking">Ranking</Link>
+          <Link to="/suggestion">Suggestion</Link>
+          <Link to="/recommendation">Recommendation</Link>
+        </nav>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/suggestion" element={<Suggestion />} />
+          <Route path="/recommendation" element={<Recommendation />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
