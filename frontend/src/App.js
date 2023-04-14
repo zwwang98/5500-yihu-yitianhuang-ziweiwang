8bc9082d106acd40d3f32846964f2c8f93f17cd2
ziweiwang98 <wang.ziwei2@northeastern.edu>
2023-04-14 15:19:54 -0700

    Changed home page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import './App.css';
-import Suggestion from './Suggestion';
-import Recommendation from './Recommendation';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import "./App.css";
+import Suggestion from "./Suggestion";
+import Recommendation from "./Recommendation";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Home = () => <h1>Hello!</h1>;
 const Ranking = () => <h1>Ranking</h1>;
@@ -15,13 +15,13 @@ function App() {
     <Router>
       <div className="App container">
         <nav>
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/ranking">Ranking</Link>
           <Link to="/suggestion">Suggestion</Link>
           <Link to="/recommendation">Recommendation</Link>
         </nav>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/ranking" element={<Ranking />} />
           <Route path="/suggestion" element={<Suggestion />} />
           <Route path="/recommendation" element={<Recommendation />} />
